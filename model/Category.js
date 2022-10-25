1f7d438e2a8e7cfd4441dea6abc7ef2d247b5960
Juno2703 <111256805+Juno2703@users.noreply.github.com>
2022-10-25 20:18:00 +0700

    view Removed!

--- model/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const CategorySchema = new Schema({
-    MaCategory: String,
-    MotaCategory: String,
-});
-
-module.exports = mongoose.model("Category", CategorySchema);
